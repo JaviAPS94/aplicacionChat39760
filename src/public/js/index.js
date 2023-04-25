ed69d69ae4b79aa9eb22dc7a6fc4da3a105c3b21
alex.pinaida <alex.pinaida@rappi.com>
2023-04-25 17:56:23 -0500

    feat: chat parte 2

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,23 @@
 const socket = io();
+
+// Swal.fire({
+//     title: 'Saludos',
+//     text: 'Mensaje inicial',
+//     icon: 'success'
+// });
+
+let user;
+const chatbox = document.getElementById('chatBox');
+
+Swal.fire({
+    title: 'Identificate',
+    input: 'text',
+    text: 'Ingresa el usuario para indentificarte en el chat',
+    inputValidator: (value) => {
+        return !value && "Necesitas escribir un nombre de usuario para comenzar a chatear"
+    },
+    allowOutsideClick: false,
+    allowEscapeKey: false
+}).then(result => {
+    user = result.value;
+});
