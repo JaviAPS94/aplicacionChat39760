ccee436087665f33887bbe1078dfbb186406a542
alex.pinaida <alex.pinaida@rappi.com>
2023-04-25 18:29:38 -0500

    feat: tercera parte chat

--- src/app.js
+++ src/app.js
@@ -16,3 +16,19 @@ app.use('/', viewsRouter);
 
 const server = app.listen(8080, () => console.log('Server running'));
 const io = new Server(server);
+
+const messages = [];
+
+io.on('connection', socket => {
+    console.log('Nuevo cliente conectado');
+
+    socket.on('message', data => {
+        messages.push(data);
+        io.emit('messageLogs', messages);
+    });
+
+    socket.on('authenticated', data => {
+        socket.emit('messageLogs', messages);
+        socket.broadcast.emit('newUserConnected', data);
+    });
+})
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -20,4 +20,35 @@ Swal.fire({
     allowEscapeKey: false
 }).then(result => {
     user = result.value;
+    socket.emit('authenticated', user);
+});
+
+chatbox.addEventListener('keyup', evt => {
+    if(evt.key==='Enter') {
+        if(chatbox.value.trim().length > 0) {
+            socket.emit('message', { user, message: chatbox.value});
+            chatbox.value='';
+        }
+    }
+});
+
+socket.on('messageLogs', data => {
+    let log = document.getElementById('messageLogs');
+    let messages = '';
+    data.forEach(message => {
+        messages += `${message.user} dice: ${message.message}<br/>`
+    });
+    log.innerHTML=messages;
+});
+
+
+socket.on('newUserConnected', data => {
+    Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        title: `${data} se ha unido al chat`,
+        icon: 'success'
+    });
 });
